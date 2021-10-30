5e1764918e471494a15cf79fe89e139b9dc9a98f
Lisias T <me@lisias.net>
2021-10-30 09:05:23 -0300

    Missing miscelannea details

--- Source/AviationLights/Properties/LegalMamboJambo.cs
+++ Source/AviationLights/Properties/LegalMamboJambo.cs
@@ -3,7 +3,7 @@ namespace AviationLights
 {
 	public static class LegalMamboJambo
 	{
-		public const string Company = "L Aerospace KSP Division";
+		public const string Company = "/L Aerospace KSP Division";
 		public const string Copyright = "© 2021 LisiasT";
 		public const string Product = "AviationLights";
 		public const string Trademark = "Position/Navigation Lights™ RPGprayer; Aviation Lights™ by MOARdV, BigNose; /L Unleashed by LisiasT";
