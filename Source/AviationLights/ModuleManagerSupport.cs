dfd7545a9cf8bf577a5567e861c1d6a4cf137557
Lisias T <me@lisias.net>
2023-01-29 23:47:01 -0300

    Replacing KSPe's MM tags for KSP for a custom solution.

---
+++ Source/AviationLights/ModuleManagerSupport.cs
@@ -0,0 +1,33 @@
+﻿/*
+	This file is part of Aviation Lights /L
+		© 2021-2023 LisiasT : http://lisias.net <support@lisias.net>
+
+	Aviation Lights /L is licensed as follows:
+		* CC-BY-NC-SA 4.0i : https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+	Aviation Lights /L is distributed in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+	warranty of	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+*/
+using System.Collections.Generic;
+using IO = KSPe.IO;
+using Directory = KSPe.IO.Directory;
+using Path = KSPe.IO.Path;
+
+namespace HLAirshipsCore
+{
+	public class ModuleManagerSupport : UnityEngine.MonoBehaviour
+	{
+		public static IEnumerable<string> ModuleManagerAddToModList()
+		{
+			List<string> list = new List<string>();
+			if (KSPe.Util.KSP.Version.Current >= KSPe.Util.KSP.Version.FindByVersion(1,4,0))
+				list.Add("AviationLights-KSP-1.4");	
+			if (KSPe.Util.KSP.Version.Current >= KSPe.Util.KSP.Version.FindByVersion(1,11,0))
+				list.Add("AviationLights-KSP-1.11");	
+			string[] r = list.ToArray();
+			return r;
+		}
+	}
+}
